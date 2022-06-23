2ae88c2d7e4079db56555490bf9901007d99bf0a
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-06-23 08:10:12 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask virtualbox
+brew install git
+brew install vagrant
 
