96d1a455927e0f9b4f23e0e2a1e34775010a10f8
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-06-23 08:12:11 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -3,5 +3,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install --cask virtualbox
 brew install git
+brew install nmap
 brew install vagrant
+brew install --cask wireshark
+brew install --cask google-chrome
+brew install --cask owasp-zap
+brew install --cask burp-suite
+
 
