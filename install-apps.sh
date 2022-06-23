435e919a20f176ed05444d54ed5926b7e86c82e4
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-06-23 10:11:10 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -10,10 +10,10 @@ brew install --cask wireshark
 brew install --cask google-chrome
 brew install --cask owasp-zap
 brew install --cask burp-suite-professional
-vagrant down
 vagrant box add kalilinux/rolling --provider virtualbox
 mkdir -p vagrant-projects/kali
 cd vagrant-projects/kali
+vagrant down
 vagrant init kalilinux/rolling
 read -p "IMPORTANT! You must enable the Virtual Box Plugin Under System Preferences - Security & Privacy - General. Don't proceed until complete"
 vagrant up
