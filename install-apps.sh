87e6e6a56725b081b32d6fa4f7016d76811beeed
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-06-23 09:00:38 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://github.com/paulkwalton/macos-hardening/blob/main/macos_provisioning_script.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/paulkwalton/macos-hardening/main/macos_provisioning_script.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install --cask virtualbox
 brew install git
