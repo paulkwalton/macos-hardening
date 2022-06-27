3cb4d7e6a30de7632ef2c54a84c449dc31053844
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-06-27 10:39:37 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/paulkwalton/macos-hardening/main/macos_provisioning_script.sh)"
 softwareupdate -i -a
+for i in `vagrant global-status | grep virtualbox | awk '{ print $1 }'` ; do vagrant destroy $i ; done
+rm -r vagrant-projects
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install --cask virtualbox
 brew install --cask vmware-fusion
@@ -14,8 +16,6 @@ brew install --cask firefox
 brew install --cask owasp-zap
 brew install --cask burp-suite-professional
 brew install --cask tenable-nessus-agent
-rm -r vagrant-projects
-vagrant destroy -f
 vagrant box add kalilinux/rolling --provider virtualbox
 mkdir -p vagrant-projects/kali
 cd vagrant-projects/kali
